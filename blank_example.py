782bc8db2e1fc777f17f3c5ebdd6defce01bc02b
Adrien Treuille <adrien.g.treuille@gmail.com>
2021-03-03 17:05:14 -0800

    Added a blank example of this thing so that I could play around with various event models.

---
+++ blank_example.py
@@ -0,0 +1,25 @@
+"""
+# Non-trivial state intialization
+
+This is an example where we're trying to initialize the state properly
+
+## Notes
+
+- **Adrien will write notes and observations here.**
+"""
+
+import streamlit as st
+import st_event
+
+def example_with_callbacks():
+    """
+    - **Adrien will write notes and observations here.**
+    """
+    st.write("example_with_callbacks")
+
+
+def example_with_signals():
+    """
+    - **Adrien will write notes and observations here.**
+    """
+    st.write("example_with_signals")
