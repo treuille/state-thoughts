18dab27bfdca76302e9aff6909f17d364ba5526b
Adrien Treuille <adrien.g.treuille@gmail.com>
2021-03-11 09:37:32 -0800

    Working with Amanda's new prototype

--- linked_sliders.py
+++ linked_sliders.py
@@ -22,7 +22,7 @@ Here we are trying to get two sliders to be linked together.
 import streamlit as st
 
 # Convert between fahrenheit and celsius
-to_celcius = lambda fahrenheit: (fahrenheit - 32) * 5.0 / 9.0
+to_celsius = lambda fahrenheit: (fahrenheit - 32) * 5.0 / 9.0
 to_fahrenheit = lambda celsius: 9.0 / 5.0 * celsius + 32
 
 MIN_CELCIUS, MAX_CELCIUS = -100.0, 100.0
@@ -42,28 +42,28 @@ def example_with_callbacks():
     - This example failed for me:
       ```py
       state = st.beta_session_state() 
-      if 'temperature_celcius' not in dir(state):
-          state.temperature_celcius = MIN_CELCIUS
+      if 'temperature_celsius' not in dir(state):
+          state.temperature_celsius = MIN_CELCIUS
       ```
       With the following message: *StreamlitAPIException: Session state
-      variable has not been initialized: "temperature_celcius"*. Is this really
+      variable has not been initialized: "temperature_celsius"*. Is this really
       what we want??
     """
     # Get and initialize the state.
-    state = st.beta_session_state(temperature_celcius=MIN_CELCIUS) 
+    state = st.beta_session_state(temperature_celsius=MIN_CELCIUS) 
 
     # Callbacks if something changes
-    def celcius_changed(new_celcius_temperature):
-        state.temperature_celcius = new_celcius_temperature
+    def celsius_changed(new_celsius_temperature):
+        state.temperature_celsius = new_celsius_temperature
 
     def fahrenheit_changed(new_fahrenheit_temperature):
-        state.temperature_celcius = to_celcius(new_fahrenheit_temperature)
+        state.temperature_celsius = to_celsius(new_fahrenheit_temperature)
 
     celsius = st.slider("Celsius", MIN_CELCIUS, MAX_CELCIUS,
-            state.temperature_celcius, on_change=celcius_changed)
+            state.temperature_celsius, on_change=celsius_changed)
     fahrenheit = st.slider("Fahrenheit", to_fahrenheit(MIN_CELCIUS),
             to_fahrenheit(MAX_CELCIUS), 
-            to_fahrenheit(state.temperature_celcius),
+            to_fahrenheit(state.temperature_celsius),
             on_change=fahrenheit_changed)
 
     st.write(f"`{celsius}c` == `{fahrenheit}f`")
@@ -77,28 +77,30 @@ def example_with_signals():
       kinda weird how you're juggling these two pieces of state (fahrenheit and
       celsius) in a tricky way.
     """
-    # Let's add some signal handlers here
-    if st.beta_signal("Celsius"):
-        celsius = st.beta_signal_value()
-        fahrenheit = to_fahrenheit(celsius)
-    elif st.beta_signal("Fahrenheit"):
-        fahrenheit = st.beta_signal_value()
-        celsius = to_celcius(fahrenheit)
+    # Get the state
+    state = st.get_state()
+
+    # Convert the range bounds to fahrenheit.
+    min_fahrenheit = to_fahrenheit(MIN_CELCIUS)
+    max_fahrenheit = to_fahrenheit(MAX_CELCIUS)
+
+    # Show the old state
+    st.write(f"`{state.celsius}`c == `{state.fahrenheit}`f")
+
+    # React to any changes up here
+    if st.widget_changed("celsius"):
+        st.error("celsius changed")
+        state.fahrenheit = to_fahrenheit(state.celsius)
+    elif st.widget_changed("fahrenheit"):
+        st.error("fahrenheit changed")
+        state.celsius = to_celsius(state.fahrenheit)
     else:
-        celsius, fahrenheit = None, None
+        st.success("nothing changed")
 
     # Now actually display the sliders
-    celsius = st.slider("Celsius", MIN_CELCIUS, MAX_CELCIUS, celsius, signal="Celsius")
-    fahrenheit = st.slider("Fahrenheit", to_fahrenheit(MIN_CELCIUS),
-            to_fahrenheit(MAX_CELCIUS), fahrenheit, signal="Fahrenheit")
+    st.slider("Celsius", MIN_CELCIUS, MAX_CELCIUS, key="celsius")
+    st.slider("Fahrenheit", min_fahrenheit, max_fahrenheit, key="fahrenheit")
 
-    st.write(f"`{celsius}`c == `{fahrenheit}`f")
+    # Show the new state
+    st.write(f"`{state.celsius}`c == `{state.fahrenheit}`f")
 
-    # Note that launching the balloons resets the sliders. I think we need to
-    # consider something like DONT_CHANGE for linked sliders.
-    st.warning(
-        "☣️ Clicking the button below will mess up the state. "
-        "We need to fix this.")
-    if st.beta_signal("Balloons"):
-        st.balloons()
-    st.button("Balloons", signal="Balloons")
