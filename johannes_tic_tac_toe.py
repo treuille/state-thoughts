6e63e3a1d2386d2202596f638053d0a09ab3ca1e
Johannes Rieke <johannes.rieke@gmail.com>
2021-03-12 20:07:27 +0100

    Stub for tic tac toe

--- johannes_tic_tac_toe.py
+++ johannes_tic_tac_toe.py
@@ -4,11 +4,19 @@
 
 import streamlit as st
 
+
 def example_with_callbacks():
     """
     - **Adrien will write notes and observations here.**
     """
-    st.write("example_with_callbacks")
+    state = st.beta_session_state(
+        fields=[[None, None, None], [None, None, None], [None, None, None]]
+    )
+
+    cols = st.beta_columns(3)
+    for i, row in enumerate(state.fields):
+        for j, field in enumerate(row):
+            cols[j].button(".", key=f"{i}-{j}")
 
 
 def example_with_signals():
